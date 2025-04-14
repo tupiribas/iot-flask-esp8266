d45405cb3548a10246b0fee8897efd1156f900fb
tupiribas <tupiguedes@outlook.com>
2025-04-14 03:34:12 -0300

    Implementação de telemetria

--- cliente/main.py
+++ cliente/main.py
@@ -1,7 +1,9 @@
 import network
+import gc
 import urequests
 import time
 import json
+from machine import Pin
 
 
 def load_config(filename="config.txt"):
@@ -32,6 +34,8 @@ SERVER_PORT = config.get('SERVER_PORT')
 # Endereço do servidor Flask
 SERVER_URL = f'http://{SERVER_IP}:{SERVER_PORT}/data'
 
+led = Pin(2, Pin.OUT)
+
 
 def connect_wifi():
     sta_if = network.WLAN(network.STA_IF)
@@ -42,18 +46,23 @@ def connect_wifi():
         while not sta_if.isconnected():
             time.sleep(2)
         print('Conectado ao WiFi. IP:', sta_if.ifconfig()[0])
-        return sta_if
     else:
         print('Já conectado ao WiFi. IP:', sta_if.ifconfig()[0])
-        return sta_if
+    return sta_if
 
 
 def send_data(data):
     try:
+        led.value(1)  # Acende o led antes de enviar
         headers = {'Content-Type': 'application/json'}
         response = urequests.post(SERVER_URL, json=data, headers=headers)
         print('Resposta do servidor:', response.status_code, response.text)
         response.close()
+        led.value(0)  # Apaga o LED após receber a resposta (ou falhar)
+        time.sleep(0.1)  # Pequena pausa para o flash ser visível
+        led.value(1)  # Prepara para o próximo envio
+        time.sleep(0.1)
+        led.value(0)
     except Exception as e:
         print('Erro ao enviar dados:', e)
 
@@ -61,14 +70,32 @@ def send_data(data):
 # Conectar ao WiFi
 wifi = connect_wifi()
 
-# Exemplo de dados a serem enviados
-sensor_data = {
-    'temperatura': 25.5,
-    'umidade': 60.2,
-    'leitura': 123
-}
 
-# Enviar os dados periodicamente
+def get_wifi_info():
+    sta_if = network.WLAN(network.STA_IF)
+    if sta_if.isconnected():
+        return {
+            "Conectado": True,
+            "Nome_Rede": sta_if.config('essid'),
+            "Sinal_wifi": sta_if.status('rssi'),
+            "IP": sta_if.ifconfig()[0],
+            "Endereco_MAC": sta_if.config('mac').hex(':')
+        }
+    else:
+        return {"Conectado": False}
+
+
+def get_memory_info():
+    return {
+        "Memoria_Livre": gc.mem_free(),
+        "Memoria_Usada": gc.mem_alloc()
+    }
+
+
 while True:
-    send_data(sensor_data)
+    data = {
+        "wifi_info": get_wifi_info(),
+        "memoria_info": get_memory_info()
+    }
+    send_data(data)
     time.sleep(5)  # Envia dados a cada 5 segundos
